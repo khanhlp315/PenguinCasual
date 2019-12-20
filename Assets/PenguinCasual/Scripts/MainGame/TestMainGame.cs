4ad9eae2ad0176c1ce1acfeb96af6f6229dcfc87
Loc Nguyen <nguyentanloc109@gmail.com>
2019-12-20 21:56:28 +0700

    Add deadzone + wall

--- Assets/PenguinCasual/Scripts/MainGame/TestMainGame.cs
+++ Assets/PenguinCasual/Scripts/MainGame/TestMainGame.cs
@@ -22,12 +22,21 @@ namespace Penguin
         void OnPlayerCollideWithPedestal(Pedestal pedestal)
         {
             // If character is in middle air, and player rotate the platform and causes collision, this threshold will prevent the character bounce back
-            if (_character.transform.position.y - pedestal.transform.position.y > -0.3f)
+            if (_character.transform.position.y - pedestal.transform.position.y < -0.3f)
+                return;
+
+            if (pedestal.type == PedestalType.Pedestal_01)
             {
                 _character.Jump();
-                Debug.Log(_character.transform.position.y - pedestal.transform.position.y);
             }
-                
+            else if (pedestal.type == PedestalType.DeadZone_01)
+            {
+                Debug.Log("Dead by touching deadzone");
+            }
+            else if (pedestal.type == PedestalType.Wall_01)
+            {
+                Debug.Log("Dead by touching wall");
+            }
         }
     }
 }
